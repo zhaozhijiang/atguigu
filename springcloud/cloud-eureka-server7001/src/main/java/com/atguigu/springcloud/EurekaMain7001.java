9124da1e8f45103fba9ad8912deca001380d1382
443309736@qq.com <Zhanghuan@520>
2023-02-05 20:13:19 +0800

    eurekaServer端服务注册中心

---
+++ springcloud/cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhijiang.zhao
+ * @date 2023/2/5 20:05
+ * @description: TODO
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
