29cef1581b6958b82889994f741f1197d517d090
443309736@qq.com <Zhanghuan@520>
2023-02-07 21:31:51 +0800

    SpringCloud Hystrix 断路器

---
+++ springcloud/cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author zhijiang.zhao
+ * @date 2023/2/7 21:26
+ * @description: TODO
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
