0e6a0b6e13c857b10d564ccae324c9737866b468
zhaozhijiangwork <zhaozhijiangwork@126.com>
2023-02-16 16:39:32 +0800

    SpringCloud Bus 消息总线

---
+++ springcloud/cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author: zhijiang.zhao
+ * @date: 2023/2/16 14:25
+ * @Description:
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
