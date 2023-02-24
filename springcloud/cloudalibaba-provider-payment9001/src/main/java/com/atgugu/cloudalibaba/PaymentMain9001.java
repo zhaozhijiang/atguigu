4820afcacdfb2fe694a0826c1e84837a143a9079
zhaozhijiangwork <zhaozhijiangwork@126.com>
2023-02-24 17:29:12 +0800

    springcloud-alibaba:nacos

---
+++ springcloud/cloudalibaba-provider-payment9001/src/main/java/com/atgugu/cloudalibaba/PaymentMain9001.java
@@ -0,0 +1,18 @@
+package com.atgugu.cloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: zhijiang.zhao
+ * @date: 2023/2/24 15:58
+ * @Description:
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
